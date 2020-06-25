fcef4b18d603a6ace7d0d8609ea18123c93aec0e
talafek96 <talafek@campus.technion.ac.il>
2020-06-25 16:44:02 +0300

    Fix _iterator class (missing const declarations)

--- part_a/IntMatrix.h
+++ part_a/IntMatrix.h
@@ -178,7 +178,7 @@ namespace mtm {
              * ---------------------------------------
              * Copies an existing iterator.
              */
-            _iterator(_iterator& it) : matrix(it.matrix), index(it.index) { }
+            _iterator(const _iterator& it) : matrix(it.matrix), index(it.index) { }
             
             /*
              * Operator: =
@@ -240,12 +240,12 @@ namespace mtm {
              * Returns a bool value that determines whether it1 is equal to it2 (true or false
              * according to the used operator).
              */
-            bool operator==(_iterator& it)
+            bool operator==(const _iterator& it) const
             {
                 return (index == it.index) && (matrix == it.matrix);
             }
             
-            bool operator!=(_iterator& it)
+            bool operator!=(const _iterator& it) const
             {
                 return !(*this == it);
             }
